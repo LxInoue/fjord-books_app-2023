305c37216da7faceeddf6423b5ebdadeea5fcb03
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-17 17:18:16 +0900

    不要行を削除

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # TODO 提出時は:ja
-    config.i18n.default_locale = :ja
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
