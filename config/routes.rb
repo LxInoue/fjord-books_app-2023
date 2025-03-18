7ed8bb18647507d783856b77d3c45e85307de2d7
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-18 16:21:29 +0900

    ユーザー一覧画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'books#index'
   devise_for :users
   resources :books
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
