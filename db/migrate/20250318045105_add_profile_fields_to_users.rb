760f7dc63751d61c6e20b87381f04a8695207c71
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-19 13:07:16 +0900

    usersにテーブルを追加

--- db/migrate/20250318045105_add_profile_fields_to_users.rb
+++ db/migrate/20250318045105_add_profile_fields_to_users.rb
@@ -1,5 +1,6 @@
 class AddProfileFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :name, :string, null: false, default: ""
     add_column :users, :postal_code, :string
     add_column :users, :address, :string
     add_column :users, :bio, :text
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_18_045105) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.string "postal_code"
     t.string "address"
     t.text "bio"
