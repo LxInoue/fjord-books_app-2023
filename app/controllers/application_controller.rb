3735d5081a4c83ea3712be63b2d03bff5d555899
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-19 13:47:37 +0900

    画面に名前の項目を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:postal_code, :address, :bio]
+    added_attrs = [:name, :postal_code, :address, :bio]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
