ef81cc7557c3b26e6178f9c450802036418bd385
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-19 17:28:12 +0900

    ユーザー情報が更新されない不具合の対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  def update_without_password(params)
+    params.delete(:password)
+    params.delete(:password_confirmation)
+    update(params)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'books#index'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :books
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
