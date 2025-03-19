6b210dda59ec3997151c50e8b81870880bc6e61b
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-19 17:42:06 +0900

    letter_openerの設定を追加

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-
+config.action_mailer.delivery_method = :letter_opener_web
+config.action_mailer.perform_deliveries = true
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   }
   resources :books
   resources :users, only: [:index, :show]
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
