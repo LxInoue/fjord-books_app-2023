3e428ca5bdc343008047bfbcdd38ce9572529568
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-24 17:49:03 +0900

    本にコメントする機能を追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  before_action :set_commentable
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.comment.notice_create', name: Comment.model_name.human)
+    else
+      redirect_to @commentable, alert: t('controllers.comment.alert_create_failed')
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      redirect_to @commentable, alert: t('controllers.comment.alert_no_permission')
+    end
+  end
+
+  private
+
+  def set_commentable
+    resource, id = request.path.split('/')[1,2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
