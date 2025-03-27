52424ae06123a05003574d97ec183767d4cc321f
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-27 14:02:00 +0900

    user_testのテストコードを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name_or_emailは名前またはメールを返す" do
+    alice = users(:alice)
+    bob = users(:bob)
+    bob.name = ''
+
+    assert_equal alice.name, alice.name_or_email
+    assert_equal bob.email, bob.name_or_email
+  end
 end
