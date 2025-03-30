335ecf3b50bcf54677b595dbb5af043d51dfb123
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-31 00:19:22 +0900

    report_paramsからuser_idを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -54,6 +54,6 @@ class ReportsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def report_params
-    params.require(:report).permit(:title, :content, :user_id)
+    params.require(:report).permit(:title, :content)
   end
 end
