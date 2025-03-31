d2f488713b0317f5c97f4424d92a454943179f75
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-31 17:31:20 +0900

    モデルにバリデーションを追加

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -3,4 +3,6 @@
 class ReportMention < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
+
+  validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
