fc3ddb4a31618c15d2536808445460339d8a722f
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-31 16:08:48 +0900

    言及の向きが逆になっていたため修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,24 +4,24 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :report_mentions_as_mentioning,
+  has_many :report_mentions_as_source,
            class_name: 'ReportMention',
            foreign_key: 'mentioning_report_id',
            dependent: :destroy,
            inverse_of: :mentioning_report
 
-  has_many :mentioned_reports,
-           through: :report_mentions_as_mentioning,
+  has_many :mentioning_reports,
+           through: :report_mentions_as_source,
            source: :mentioned_report
 
-  has_many :report_mentions_as_mentioned,
+  has_many :report_mentions_as_target,
            class_name: 'ReportMention',
            foreign_key: 'mentioned_report_id',
            dependent: :destroy,
            inverse_of: :mentioned_report
 
-  has_many :mentioning_reports,
-           through: :report_mentions_as_mentioned,
+  has_many :mentioned_reports,
+           through: :report_mentions_as_target,
            source: :mentioning_report
 
   validates :title, presence: true
